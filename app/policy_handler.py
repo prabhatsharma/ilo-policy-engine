8c2b45408ee368684628e2a0eea5c9dc95989b89
Prabhat Sharma <hi.prabhat@gmail.com>
2021-06-13 12:25:32 +0000

    rule evaluation is working

--- app/policy_handler.py
+++ app/policy_handler.py
@@ -1,11 +1,33 @@
 import json
 
+policies = []
+
 
 def create_update(request_object):
-    print("Got new admission policy: ")
-    print(json.dumps(request_object))
+    # print("Got new admission policy: ")
+    # print(json.dumps(request_object))
+
+    print("Got new admission policy. Rules are: ")
+
+    rules = request_object["spec"]["rules"]
+
+    # print(json.dumps(rules))
+
+    for rule in rules:
+        print(rule)
+        policies.append(rule)
 
 
-def resource(request_object):
+def resource(resource):
+    print("Policies are: ", policies)
     print("Got new resource: ")
-    print(json.dumps(request_object))
+    # print(json.dumps(resource))
+
+    rule1 = policies[0]["rule"]
+    print(rule1)
+
+    rule1_result = eval(rule1, {}, { "resource": resource})
+
+    print("Result is: ", rule1_result)
+
+
